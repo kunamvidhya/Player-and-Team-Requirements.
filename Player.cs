b71bd217d5d325e1af72dbe13d1670dd2ec14f25
kunamvidhya <153719078+kunamvidhya@users.noreply.github.com>
2024-01-11 16:07:05 +0530

    Add files via upload

---
+++ Player.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Player_and_Team_Requirements
+{
+
+    public class Player
+    {
+
+
+        public int PlayerId { get; set; }
+
+        public string PlayerName { get; set; }
+
+        public int PlayerAge { get; set; }
+
+
+
+    }
+}
